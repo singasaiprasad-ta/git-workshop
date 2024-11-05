19dcda1abc41a972a56a5701cbc46f19ff2d6440
jajipravallika-ta <jajipravallika.g@tigeranalytics.com>
2024-11-05 12:06:11 +0530

    Update simple_functions.py

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,21 @@
 # Custom python functions
 
 def double_number(a):
+"""
+Returns the double of the given number
+Parameters:
+    a (int or float): The number to double.
+Returns:
+    int or float: The result of doubling the input number.
+"""
     return a+a
+"""
+Returns the square of the given number.
+Parameters:
+    a (int or float): The number to square.
+Returns:
+    int or float: The result of squaring the input number.
+"""
 
 def square_number(a):
     return a*a
