10ceab8991b57f9db819f2185249a34e318a0517
singasaiprasad-ta <singasai.prasad@tigeranalytics.com>
2024-11-06 10:53:59 +0000

    everything is ok

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mypackage",
+    version="0.0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "requests",
+        'importlib-metadata; python_version>="3.10"',
+    ],
+)
