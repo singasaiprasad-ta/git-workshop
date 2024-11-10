59062f695252dd34261444b82bcc4b965484445d
singasaiprasad-ta <singasai.prasad@tigeranalytics.com>
2024-11-10 06:44:00 +0000

    test_build.py is created

--- src/my_package/nonstandardcode.py
+++ src/my_package/nonstandardcode.py
@@ -33,8 +33,6 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 
 fetch_housing_data()
 
-fetch_housing_data()
-
 
 def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
