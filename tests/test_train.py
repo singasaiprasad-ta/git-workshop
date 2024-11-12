e2a813697b828a15e07085fb81acb44493a2260a
singasaiprasad-ta <singasai.prasad@tigeranalytics.com>
2024-11-12 11:43:01 +0000

    test_score and test_train files are updated

--- tests/test_train.py
+++ tests/test_train.py
@@ -8,7 +8,7 @@ def test_train_import():
         training = importlib.import_module("my_package.train")
 
         # Ensure that the functions exist in the module
-        fun_1 = getattr(training, "prepare_date", None)
+        fun_1 = getattr(training, "prepare_data", None)
         fun_2 = getattr(training, "train_models", None)
 
         if fun_1 is None or fun_2 is None:
